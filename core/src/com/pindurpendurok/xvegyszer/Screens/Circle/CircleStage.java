1ca2ac7544a9eec95b5a8b859fc8d9231c92040e
marton552 <marton552@gmail.com>
2020-01-31 17:15:36 +0100

    ooooh VELI OF PLENTIII

--- core/src/com/pindurpendurok/xvegyszer/Screens/Circle/CircleStage.java
+++ core/src/com/pindurpendurok/xvegyszer/Screens/Circle/CircleStage.java
@@ -2,7 +2,15 @@ package com.pindurpendurok.xvegyszer.Screens.Circle;
 
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.pindurpendurok.xvegyszer.Elements.SimpleLabel;
+import com.pindurpendurok.xvegyszer.MyGdxGame;
+import com.pindurpendurok.xvegyszer.Screens.Actors.Tolvaj;
+import com.pindurpendurok.xvegyszer.Screens.Draw.DrawScreen;
+import com.pindurpendurok.xvegyszer.Screens.End.EndScreen;
+import com.pindurpendurok.xvegyszer.Screens.House.HouseScreen;
+import com.pindurpendurok.xvegyszer.SimpleLoadingStage;
 
 import hu.csanyzeg.master.MyBaseClasses.Assets.AssetList;
 import hu.csanyzeg.master.MyBaseClasses.Game.MyGame;
@@ -16,9 +24,15 @@ import hu.csanyzeg.master.MyBaseClasses.Timers.Timer;
 public class CircleStage extends SimpleWorldStage {
 
     public static final String LABDA = "draw/null.png";
+    public static final String BACK = "circle_lock/Iron_Door_Covering.png";
+    public static final String AJTO1 = "circle_lock/Iron_Door_TwoColor.png";
+    public static final String AJTO2 = "circle_lock/Iron_Door_Red.png";
     public static AssetList list = new AssetList();
     static {
         list.addTexture(LABDA);
+        list.addTexture(BACK);
+        list.addTexture(AJTO1);
+        list.addTexture(AJTO2);
     }
 
     public float rotation = 0;
@@ -28,16 +42,38 @@ public class CircleStage extends SimpleWorldStage {
     CircleActor asd;
     CircleActor asd2;
     CircleActor asd3;
+    public boolean first = true;
 
     public CircleStage(MyGame game) {
         super(new ResponseViewport(1080f), game);
 
+        final SimpleLabel text = new SimpleLabel(game,"");
+        text.setFontScale(3f);
+        text.setPosition(getWidth()*0.5f,getHeight()*0.45f);
+        addActor(text,-1);
+        text.setAlignment(Align.center);
         asd = new CircleActor(game,world,this,0);
         addActor(asd);
         asd2 = new CircleActor(game,world,this,1);
         addActor(asd2);
         asd3 = new CircleActor(game,world,this,2);
         addActor(asd3);
+
+        final OneSpriteStaticActor ajto1 = new OneSpriteStaticActor(game,AJTO1);
+        ajto1.setSize(getWidth(),getHeight());
+        ajto1.setPosition(0,0);
+        addActor(ajto1,0);
+
+        final OneSpriteStaticActor ajto2 = new OneSpriteStaticActor(game,AJTO2);
+        ajto2.setSize(getWidth(),getHeight());
+        ajto2.setPosition(0,0);
+        addActor(ajto2,0);
+        ajto2.setVisible(false);
+
+        OneSpriteStaticActor back = new OneSpriteStaticActor(game,BACK);
+        back.setSize(getWidth(),getHeight());
+        back.setPosition(0,0);
+        addActor(back,0);
 
         TickTimer x = new TickTimer(0, true, new TickTimerListener() {
             @Override
@@ -52,17 +88,69 @@ public class CircleStage extends SimpleWorldStage {
                 else if(melyik == 2){
                     asd3.setRotation(asd3.getRotation()-2);
                 }
-                else if(melyik == 3 && (asd3.getRotation() == asd2.getRotation() && asd3.getRotation() == asd.getRotation() || asd3.getRotation()%60==0 && asd2.getRotation()%60==0 &&asd.getRotation()%60==0 )){
+                else if(melyik == 3 && Math.abs(asd.getRotation()%360 - asd2.getRotation()%360) < 20 && Math.abs(asd3.getRotation()%360 - asd2.getRotation()%360) < 20){
+                    if(first){
+                    asd.setRotation((int)Math.floor(asd.getRotation()/30)*30);
+                    asd2.setRotation(asd.getRotation());
+                    asd3.setRotation(asd.getRotation());
+                    first = false;}
                     asd.setRotation(asd.getRotation()-2);
-                    asd2.setRotation(asd2.getRotation()-2);
-                    asd3.setRotation(asd3.getRotation()-2);
+                    asd2.setRotation(asd.getRotation());
+                    asd3.setRotation(asd.getRotation());
                 }
-                System.out.println(asd.getRotation()%360);
-                System.out.println(asd2.getRotation()%360);
-                System.out.println(asd3.getRotation()%360);
-            }
+                else {
+                    getGame().setScreenWithPreloadAssets(EndScreen.class, new SimpleLoadingStage(getGame()));
+                }
+                //System.out.println(asd.getRotation()%360);
+                //System.out.println(asd2.getRotation()%360);
+                //System.out.println(asd3.getRotation()%360);
+                System.out.println(melyik);
+                if(melyik > 3) {
+                    System.out.println(asd.getRotation()%360);
+                    if ((asd.getRotation()%360 < -265 && asd.getRotation()%360 > -275) || (asd.getRotation()%360 < -85 && asd.getRotation()%360 > -95)) {
+                        System.out.println(melyik+" és "+asd.getRotation());
+                        ajto1.setVisible(false);
+                        ajto2.setVisible(true);
+                        TickTimer w = new TickTimer(0.7f, false, new TickTimerListener() {
+                            @Override
+                            public void onStop(Timer sender) {
+                                super.onStop(sender);
+                                asd.setVisible(false);
+                                asd2.setVisible(false);
+                                asd3.setVisible(false);
+                                text.setText("Kód első része:\r\n"+MyGdxGame.getPassPart(1));
+                                TickTimer w = new TickTimer(0, true, new TickTimerListener() {
+                                    @Override
+                                    public void onTick(Timer sender, float correction) {
+                                        super.onTick(sender, correction);
+                                        ajto2.setX(ajto2.getX() - 1);
+                                        if (ajto2.getX() + ajto2.getWidth() * 1.8f < 0) {
+
+                                            TickTimer w = new TickTimer(3, false, new TickTimerListener() {
+                                                @Override
+                                                public void onStop(Timer sender) {
+                                                    super.onStop(sender);
+                                                    Tolvaj.pos = 2;
+                                                    getGame().setScreenWithPreloadAssets(HouseScreen.class, new SimpleLoadingStage(getGame()));
+
+                                                }
+                                            });
+                                            addTimer(w);
+                                        }
+                                    }
+                                });
+                                addTimer(w);
+                            }
+                        });
+                        addTimer(w);
+                    } else {
+                        System.out.println("vesztés");
+                        getGame().setScreenWithPreloadAssets(EndScreen.class, new SimpleLoadingStage(getGame()));
+                    }
+                }            }
         });
         addTimer(x);
+
 
         OneSpriteStaticActor lol = new OneSpriteStaticActor(game,LABDA);
         lol.setSize(getWidth(),getHeight());
@@ -74,13 +162,13 @@ public class CircleStage extends SimpleWorldStage {
                 super.clicked(event, x, y);
                 if(melyik == 0){
                     System.out.println(rotation);
-                    asd.setRotation((int)Math.floor(asd.getRotation()/40)*40);
+                    asd.setRotation((int)Math.floor(asd.getRotation()/30)*30);
                 }
                 if(melyik == 1){
                     asd2.setRotation((int)Math.floor(asd2.getRotation()/30)*30);
                 }
                 if(melyik == 2){
-                    asd3.setRotation((int)Math.floor(asd3.getRotation()/20)*20);
+                    asd3.setRotation((int)Math.floor(asd3.getRotation()/30)*30);
                 }
                 if(melyik == 3 ){
                     asd.setRotation((int)Math.floor(asd.getRotation()/30)*30);
--- core/src/com/pindurpendurok/xvegyszer/Screens/Draw/DrawStage.java
+++ core/src/com/pindurpendurok/xvegyszer/Screens/Draw/DrawStage.java
@@ -1,8 +1,13 @@
 package com.pindurpendurok.xvegyszer.Screens.Draw;
 
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.pindurpendurok.xvegyszer.Elements.SimpleLabel;
+import com.pindurpendurok.xvegyszer.MyGdxGame;
+import com.pindurpendurok.xvegyszer.Screens.Actors.Tolvaj;
+import com.pindurpendurok.xvegyszer.Screens.End.EndScreen;
+import com.pindurpendurok.xvegyszer.Screens.House.HouseScreen;
 import com.pindurpendurok.xvegyszer.SimpleLoadingStage;
 
 import hu.csanyzeg.master.MyBaseClasses.Assets.AssetList;
@@ -20,7 +25,8 @@ public class DrawStage extends SimpleWorldStage {
     public String eredmeny = "";
     public boolean vege =true;
     public static final String BORDER = "draw/border.png";
-    public static final String BACK = "ui_textures/gold.png";
+    public static final String BACK1 = "draw/Iron_Door_Covering.png";
+    public static final String AJTO = "draw/Room1_Door.png";
     public String[] peldak = {"21","10","03","32","210","103","032","321","2103","1032","0321","3210"};
     public final static String[] KEPEK = {"draw/Arrow_Uptoright.png","draw/Arrow_NotCircle.png","draw/Arrow_Nemkelnev.png"};
     public static AssetList list = new AssetList();
@@ -28,7 +34,8 @@ public class DrawStage extends SimpleWorldStage {
         for (int i = 0; i < KEPEK.length; i++) {
             list.addTexture(KEPEK[0]);
             list.addTexture(BORDER);
-            list.addTexture(BACK);
+            list.addTexture(BACK1);
+            list.addTexture(AJTO);
         }
     }
     public boolean end = false;
@@ -39,20 +46,33 @@ public class DrawStage extends SimpleWorldStage {
     public int plus = 0;
     float szeles = 0;
     public boolean nyert = false;
+    OneSpriteStaticActor ajto1;
+    SimpleLabel text1;
 
     public DrawStage(MyGame game) {
         super(new ResponseViewport(1080f), game);
-        OneSpriteStaticActor lol = new OneSpriteStaticActor(game,BACK);
-        lol.setSize(getWidth(),getHeight());
-        lol.setPosition(0,0);
-        addActor(lol);
 
         final Huzas asd = new Huzas(game,this);
-
         SimpleLabel text = new SimpleLabel(game,"5/"+asd.mennyi);
         text.setFontScale(5f);
         text.setPosition(getWidth()/2,getHeight()*0.9f);
         addActor(text);
+
+        text1 = new SimpleLabel(game,"");
+        text1.setFontScale(2.5f);
+        text1.setPosition(getWidth()*0.5f,getHeight()*0.45f);
+        addActor(text1,-1);
+        text1.setAlignment(Align.center);
+
+        ajto1 = new OneSpriteStaticActor(game,AJTO);
+        ajto1.setSize(getWidth(),getHeight());
+        ajto1.setPosition(0,0);
+        addActor(ajto1,0);
+
+        OneSpriteStaticActor back = new OneSpriteStaticActor(game,BACK1);
+        back.setSize(getWidth(),getHeight());
+        back.setPosition(0,0);
+        addActor(back,0);
 
         System.out.println(peldak[toss_a_coint_to_your_witcher]);
 
@@ -92,6 +112,7 @@ public class DrawStage extends SimpleWorldStage {
                     }
                     else{
                         vege = false;
+                        getGame().setScreenWithPreloadAssets(EndScreen.class, new SimpleLoadingStage(getGame()));
                     }
                 }
                 }
@@ -108,6 +129,7 @@ public class DrawStage extends SimpleWorldStage {
                 }}
                 else{//System.out.println("VEGE");
                     vege=false;
+                    getGame().setScreenWithPreloadAssets(EndScreen.class, new SimpleLoadingStage(getGame()));
                     }
                 }
             if(end){
@@ -115,7 +137,7 @@ public class DrawStage extends SimpleWorldStage {
                     @Override
                     public void onStop(Timer sender) {
                         super.onStop(sender);
-                        System.out.println("kövi");
+                        vege();
                     }
                 });
                 addTimer(x);
@@ -210,5 +232,33 @@ public class DrawStage extends SimpleWorldStage {
             game.setScreenWithPreloadAssets(DrawScreen.class, new SimpleLoadingStage(game));
             //lerak();
         }
+        else{
+
+        }
+    }
+
+    void vege(){
+        text1.setText("Kód második része:\r\n"+ MyGdxGame.getPassPart(2));
+        TickTimer w = new TickTimer(0, true, new TickTimerListener() {
+            @Override
+            public void onTick(Timer sender, float correction) {
+                super.onTick(sender, correction);
+                ajto1.setX(ajto1.getX() - 1);
+                if (ajto1.getX() + ajto1.getWidth() * 1.8f < 0) {
+
+                    TickTimer w = new TickTimer(3, false, new TickTimerListener() {
+                        @Override
+                        public void onStop(Timer sender) {
+                            super.onStop(sender);
+                            Tolvaj.pos = 3;
+                            getGame().setScreenWithPreloadAssets(HouseScreen.class, new SimpleLoadingStage(getGame()));
+
+                        }
+                    });
+                    addTimer(w);
+                }
+            }
+        });
+        addTimer(w);
     }
 }
